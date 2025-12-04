ee0959f1fe342e39c0997c8b6633a8a3b37a59e3
kyuuinochi <kyuuinochi@gmail.com>
2025-12-04 10:15:07 +0800

    update secret

--- llm/secret.py
+++ llm/secret.py
@@ -1,1 +1,9 @@
-OPENAI_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# secret.py
+# Store secrets here. Never commit this file to Git.
+OPENAI_API_KEY = "xxx"
+DEEPSEEK_API_KEY = "xxx"
+QWEN_API_KEY = "xxx"
+DOUBAO_API_KEY = "xxx"
+GEMINI_API_KEY = "xxx"
+GROK_API_KEY = "xxx"
+API_KEY = "xxx"
